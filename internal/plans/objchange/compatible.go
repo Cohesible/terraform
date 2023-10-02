fddb3d56b622f358564d7daa721ab0c662ea6be7
JadenSimon <simonjaden223@gmail.com>
2023-10-02 07:03:58 -0700

    Add hack

--- internal/plans/objchange/compatible.go
+++ internal/plans/objchange/compatible.go
@@ -228,7 +228,11 @@ func assertValueCompatible(planned, actual cty.Value, path cty.Path) []error {
 		return errs
 	}
 	if planned.IsNull() {
-		errs = append(errs, path.NewErrorf("was null, but now %#v", actual))
+		// XXX: this hack is currently necessary to support the `cloudscript` builtin provider
+		// the provider should map all null input values to "Unknown"
+		//
+		// errs = append(errs, path.NewErrorf("was null, but now %#v", actual))
+
 		return errs
 	}
 
