31bc184ad0cd2e47be1b44e000cc7a65fec3b6e2
JadenSimon <simonjaden223@gmail.com>
2024-06-09 20:03:12 -0700

    Fix flaky provider cache

--- internal/providercache/package_install.go
+++ internal/providercache/package_install.go
@@ -300,7 +300,9 @@ func installFromLocalDir(ctx context.Context, meta getproviders.PackageMeta, tar
 	}
 
 	err = os.Symlink(linkTarget, absNew)
-	if err == nil {
+
+	// TODO: determine why we sometimes see "fileexists" in CI
+	if err == nil || os.IsExist(err) {
 		// Success, then!
 		return nil, nil
 	}
